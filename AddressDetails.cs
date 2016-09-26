7b8c186740d0631d2442013621b497586e57e18e
harishsune <harishsune@hotmail.com>
2016-09-26 21:58:54 +0530

    Add project files.

---
+++ AddressDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    public class AddresssDetails
+    {
+        [XmlAttribute("Number")]
+         public int HouseNo { get; set; }
+        [XmlElement("Street")]
+        public string StreetName { get; set; }
+        [XmlElement("CityName")]
+        public string City { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            AddresssDetails details = new AddresssDetails();
+            details.HouseNo = 4;
+            details.StreetName = "Not available";
+            details.City = "Pune";
+            Serialize(details);
+        }
+        static public void Serialize(AddresssDetails details)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(AddresssDetails));
+            using (TextWriter writer = new StreamWriter(@"d:\XmlSerialize.xml"))
+            {
+                serializer.Serialize(writer, details);
+            }
+        }
+    }
+}
